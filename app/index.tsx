1634121eaf80a296b07d251a4e12878e87b7873e
gokuljs <gokuljs029@gmail.com>
2024-09-15 01:03:18 +0530

    routing and font

--- app/index.tsx
+++ app/index.tsx
@@ -4,7 +4,7 @@ import { Link } from "expo-router";
 export default function App() {
   return (
     <View className="flex-1 items-center justify-center h-[100%] border">
-      <Text className="color-[red]">Hi man</Text>
+      <Text className="text-3xl font-black">Hi man</Text>
       <Link href="/Profile">G0 to profile</Link>
     </View>
   );
